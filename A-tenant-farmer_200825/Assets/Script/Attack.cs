f1cef7ff3483630711a0b0a016fe52d7e60a7ef0
cckrid <63984630+cckrid@users.noreply.github.com>
2020-09-09 02:28:16 +0900

    200901~200908

--- A-tenant-farmer_200825/Assets/Script/Attack.cs
+++ A-tenant-farmer_200825/Assets/Script/Attack.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    Animator anim;    
-    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트    
+    Animator anim;
+    public GameObject enemy;
+    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트       
     public int plusNum= 0;
     public int AttackNum = 0;
-    
+    private MonsterControll monster;
+    private bool Hit;
+    private bool isDead;
+    public enum PlayerState { idle, walk, attack1, attack2, attack3, dead, damaged };
+    public PlayerState playState = PlayerState.idle;
 
     void Start()
     {
@@ -19,6 +24,7 @@ public class Attack : MonoBehaviour
     {
         OnClick();
         cases();
+        PlayState();
     }
 
 
@@ -28,15 +34,18 @@ public class Attack : MonoBehaviour
             {                
                 plusNum = AttackNum + 1;            
                 Debug.Log("클릭공격 : " + plusNum);
+                Debug.Log("어택카운트 : " + anim.GetBool("AttackCount"));
+
+            Hit = true;
 
             }
-        else
+        else if (Input.GetButtonUp("Fire1") && anim.GetFloat("AttackCount")==3)
         {
-            anim.SetBool("isAttack", false);            
-            anim.SetFloat("AttackCount", 0);            
+            anim.SetBool("isAttack", false);
+            anim.SetFloat("AttackCount", 0);
+            Hit = false;
 
         }
-
         AttackNum = plusNum;
 
     }
@@ -46,40 +55,71 @@ public class Attack : MonoBehaviour
         switch (AttackNum)
         {
             case 1 :
-                
-                    anim.SetBool("isAttack", true);
-                    anim.SetFloat("AttackCount", 1);
-                if (anim.GetBool("isWalk") == true)
-                {
-                    anim.SetBool("isAttack", false);
-                    anim.SetFloat("AttackCount", 0);
-                    plusNum = 0;
-                    AttackNum = 0;
-                }
+                playState = PlayerState.attack1;                
                 break;
             case 2 :                
-                    anim.SetBool("isAttack", true);
-                    anim.SetFloat("AttackCount", 2);
-                if (anim.GetBool("isWalk") == true)
-                {
-                    anim.SetBool("isAttack", false);
-                    anim.SetFloat("AttackCount", 0);
-                    plusNum = 0;
-                    AttackNum = 0;
-                }
+                
+                playState = PlayerState.attack2;
                 break;
                 
             case 3 :                
-                    anim.SetBool("isAttack", true);
-                    anim.SetFloat("AttackCount", 3);
-                if (Input.GetButtonUp("Fire1"))
-                {
+                playState = PlayerState.attack3;
+                break;
+            default:
+                
                     anim.SetBool("isAttack", false);
                     anim.SetFloat("AttackCount", 0);
-                    plusNum = 0;
-                    AttackNum = 0;
-                }
-                    break;                   
+                //plusNum = 0;
+                //AttackNum = 0;      
+                break;
         }
+    }    
+
+    public void PlayState()
+    {        
+                switch (playState)
+                {
+                    case PlayerState.attack1:
+                        anim.SetBool("isAttack", true);
+                        anim.SetFloat("AttackCount", 1);                        
+                        break;
+                    case PlayerState.attack2:
+                
+                    anim.SetBool("isAttack", true);
+                    anim.SetFloat("AttackCount", 2);                                
+                        break;
+                    case PlayerState.attack3:
+                
+                    anim.SetBool("isAttack", true);
+                    anim.SetFloat("AttackCount", 3);
+                    if(anim.GetBool("isWalk") == true)                    
+                    {
+                    anim.SetBool("isAttack", false);
+                    anim.SetFloat("AttackCount", 0);
+                    AttackNum = 0;
+                    plusNum = 0;
+                    }
+
+                    break;
+                }
+            
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {        
+            MonsterControll monster = other.gameObject.GetComponent<MonsterControll>();
+            if(monster != null)
+            {
+                Debug.Log(Hit);
+                if(Hit == true)
+                {
+                    monster.TakeDamage(10);
+                    Debug.Log("칼");
+                    //Hit = false;
+                }
+                                
+            }        
+       
     }
 }
---
+++ A-tenant-farmer_200825/Assets/Script/MonsterControll.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterControll : MonoBehaviour
+{
+    public enum CurrentState { idle, trace, attack, dead, damaged};
+    public CurrentState curState = CurrentState.idle;
+    int time = 0;
+    private Attack attack;
+    private Transform _transform;
+    private Transform playerTransform;
+    private NavMeshAgent nvAgent;
+    private Animator _animator;
+    //private Animation animation;    
+
+
+    public float traceDist = 5.0f;
+    public float attackDist = 2.0f;
+    public float Hp = 30.0f;
+
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _transform = this.gameObject.GetComponent<Transform>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        _animator = this.gameObject.GetComponent<Animator>();
+
+        // 추적 대상의 위치를 설정하면 바로 추적 시작
+        // nvAgent.destination = playerTransform.position;
+
+        StartCoroutine(this.CheckState());
+        StartCoroutine(this.CheckStateForAction());
+    }
+
+    public void TakeDamage(int damage)
+    {
+        /*
+         * hp -= damage;
+         * if(hp < 0)
+         */
+        Hp = Hp - damage;
+        
+        if(Hp <= 0)
+        {
+            _animator.SetBool("isDie", true);
+            isDead = true;
+            curState = CurrentState.dead;
+            
+            time += 1;
+        }
+                
+
+    }
+
+    IEnumerator CheckState()
+    {
+        while (!isDead)
+        {
+            while (!isDead)
+            {
+                yield return new WaitForSeconds(0.2f);
+
+                float dist = Vector3.Distance(playerTransform.position, _transform.position);
+
+                if (dist<=attackDist)
+                {                  
+                    curState = CurrentState.attack;
+                }
+                else if(dist <= traceDist)
+                {                                        
+                    curState = CurrentState.trace;
+                }
+                else
+                {                                       
+                    curState = CurrentState.idle;
+                }
+            }
+        }
+    }
+
+    IEnumerator CheckStateForAction()
+    {
+        while (!isDead)
+        {
+            switch (curState)
+            {
+                case CurrentState.idle:
+                    nvAgent.Stop();
+                    _animator.SetBool("isWalk", false);
+                    _animator.SetBool("isAttack", false);
+                    _animator.SetBool("isDamaged", false);
+                    break;
+                case CurrentState.trace:
+                    _animator.SetBool("isWalk", true);
+                    nvAgent.destination = playerTransform.position;
+                    nvAgent.Resume();
+                    break;
+                case CurrentState.attack:
+                    _animator.SetBool("isAttack", true);                    
+                    break;
+                case CurrentState.damaged:
+                    break;
+                case CurrentState.dead:                    
+                    if (time >= 5)
+                    {
+                        nvAgent.Stop();
+                        gameObject.SetActive(false);                        
+                    }
+                    break;
+            }
+
+            yield return null;
+        }
+    }    
+}
